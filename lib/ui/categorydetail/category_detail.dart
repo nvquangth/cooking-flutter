fcb060416e58ff81730dbf01917350cf73dc35a9
nguyen.van.quangc <SUN-ASTERISK\nguyen.van.quangc@b121544-pc.sun-asterisk.com>
2019-09-13 10:33:06 +0700

    Update home screen

---
+++ lib/ui/categorydetail/category_detail.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class CategoryDetail extends StatefulWidget {
+  @override
+  State<CategoryDetail> createState() => _CategoreyDetailState();
+
+}
+
+class _CategoreyDetailState extends State<CategoryDetail> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Text("Category Detail");
+  }
+
+}
